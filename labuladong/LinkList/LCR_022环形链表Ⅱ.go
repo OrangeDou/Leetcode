bff8b40fe4142d8d9cc9d76a6048736aa7904dd9
OrangeDou <wenqiangsspu@163.com>
2024-07-31 10:53:08 +0800

    update: 环型链表快慢指针解法

--- labuladong/LinkList/LCR_022环形链表Ⅱ.go
+++ labuladong/LinkList/LCR_022环形链表Ⅱ.go
@@ -11,3 +11,27 @@ func detectCycle(head *ListNode) *ListNode {
 	}
 	return nil
 }
+
+func detectCycle2(head *ListNode) *ListNode {
+	var fast, slow *ListNode
+	fast, slow = head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+		if fast == slow {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	// 重新跑, 会在环的入口相遇
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
