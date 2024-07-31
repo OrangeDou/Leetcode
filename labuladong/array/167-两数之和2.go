aadd0fa48a092f92863699303f6d396317a5ad43
文强 <wenqiangsspu@163.com>
2024-07-31 23:11:53 +0800

    update: array

---
+++ labuladong/array/167-两数之和2.go
@@ -0,0 +1,21 @@
+package array
+
+func twoSum(numbers []int, target int) []int {
+	if len(numbers) <= 1 {
+		return nil
+	}
+	left, right := 0, len(numbers)-1
+	for left < right {
+		sum := numbers[right] + numbers[left]
+		if sum == target {
+			return []int{left + 1, right + 1}
+		} else if sum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return []int{-1, -1}
+
+}
