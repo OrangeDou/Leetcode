b505ef508c82ab793994e7534a3f3bacfec084d7
OrangeDou <wenqiangsspu@163.com>
2024-08-26 15:58:03 +0800

    add: 大神的思路NBPlus

--- interview/meituan/2024春招第一场/02-询问数组.go
+++ interview/meituan/2024春招第一场/02-询问数组.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var n, q int
@@ -49,4 +53,30 @@ func help(arr []int, ask [][]int) [][]int {
 		}
 	}
 	return result
+}
+
+// 牛友思路：
+const N int = 1e5 + 10
+
+func main2() {
+	in := bufio.NewReader(os.Stdin)
+	n := 1
+	q := 1
+	fmt.Fscan(in, &n, &q)
+
+	var s int64 = 0
+	var zeroNum int64 = 0
+	for i := 1; i <= n; i++ {
+		var a int64
+		fmt.Fscan(in, &a)
+		if a == 0 {
+			zeroNum++
+		}
+		s = s + a
+	}
+	for i := 0; i < q; i++ {
+		var l, r int64
+		fmt.Fscan(in, &l, &r)
+		fmt.Printf("%d %d\n", s+zeroNum*l, s+zeroNum*r)
+	}
 }
