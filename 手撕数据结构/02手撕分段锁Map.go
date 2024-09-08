e93e35b172340ab495a61613e42b28b764b1c3da
文强 <wenqiangsspu@163.com>
2024-09-08 08:53:41 +0800

    update

--- 手撕数据结构/02手撕分段锁Map.go
+++ 手撕数据结构/02手撕分段锁Map.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "sync"
 
 type Shard struct {
 	sync.RWMutex
-	data map[string][]interface{}
+	data map[string]interface{}
 }
 
 type ShardMap struct {
@@ -20,7 +17,7 @@ func Newshardmap(numsShard int) *ShardMap {
 	Shards := make([]*Shard, numsShard)
 	for i := 0; i < numsShard; i++ {
 		Shards[i] = &Shard{
-			data: make(map[string][]interface{}),
+			data: make(map[string]interface{}),
 		}
 	}
 	return &ShardMap{
@@ -29,49 +26,4 @@ func Newshardmap(numsShard int) *ShardMap {
 	}
 }
 
-// get
-func (s *ShardMap) Get(key string) ([]interface{}, bool) {
-	index := s.getShardIndex(key)
-	shard := s.shards[index]
-	shard.RLock()
-	defer shard.RUnlock()
-	value, exists := shard.data[key]
-	return value, exists
-
-}
-
-// set
-func (s *ShardMap) Set(key string, value interface{}) {
-	index := s.getShardIndex(key)
-	Shard := s.shards[index]
-	Shard.Lock()
-	defer Shard.Unlock()
-	Shard.data[key] = append(Shard.data[key], value)
-}
-
-func (s *ShardMap) getShardIndex(key string) int {
-	return int(key[0]) % s.numShard
-}
-
-func main() {
-	shardedMap := Newshardmap(10)
-
-	// Set some values
-	shardedMap.Set("key1", "value1")
-	shardedMap.Set("key2", "value2")
-
-	// Get values
-	if value, exists := shardedMap.Get("key1"); exists {
-		fmt.Println("key1:", value)
-	}
-
-	// Delete a key
-	shardedMap.Set("key2", nil)
-
-	// Try to get the deleted key
-	if value, exists := shardedMap.Get("key2"); exists {
-		fmt.Println("key2:", value)
-	} else {
-		fmt.Println("key2 does not exist")
-	}
-}
+//
